e4cd5d3d8abcf949d8059d08b679183232e350e7
dgstanoev <dim.stanoev94@gmail.com>
2021-06-15 17:21:34 +0300

    Add initial auth state & context

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Fragment } from 'react';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -23,3 +23,19 @@ const Contacts = () => {
 };
 
 export default Contacts;
+
+
+// import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// <TransitionGroup>
+//   {filtered !== null
+//     ? filtered.map((contact) => (
+//         <CSSTransition key={contact.id} timeout={500} classNames='item'>
+//           <ContactItem contact={contact} />
+//         </CSSTransition>
+//       ))
+//     : contacts.map((contact) => (
+//         <CSSTransition key={contact.id} timeout={500} classNames='item'>
+//           <ContactItem contact={contact} />
+//         </CSSTransition>
+//       ))}
+// </TransitionGroup>;
